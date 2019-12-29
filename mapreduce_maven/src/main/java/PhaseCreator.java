c616980cfeeb4bf22f8bc1c13499b7f9aba8fe02
wassim <berrariwassim@gmail.com>
2019-12-30 00:01:56 +0100

    creation of phase writable

---
+++ mapreduce_maven/src/main/java/PhaseCreator.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+
+public class PhaseCreator {
+  public static class PhaseCreatorMapper extends Mapper<Object, Text, NullWritable, PhaseWritable> {
+    PhasesWritable outValue = new PhasesWritable();
+    @Override
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String[] phase = value.toString().split(";");
+      if(phase[0].equals("start")){
+        return;
+      }
+      if(phase.length == 9){
+        try{
+          outValue.setDuration(Long.parseLong(phase[2]));
+          outValue.setPatterns(phase[3]);
+          outValue.setNpatterns(Integer.parseInt(phase[4]));
+          outValue.setJobs(phase[5]);
+          outValue.setNjobs(Integer.parseInt(phase[6]));
+          outValue.setDays(phase[7]);
+          outValue.setNdays(Integer.parseInt(phase[8]));
+        }catch (Exception e) {
+          return;
+        }
+        context.write(NullWritable.get(), outValue);
+      }
+    }
+  }
+
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "IdleFilter");
+    job.setNumReduceTasks(0);
+    job.setJarByClass(PhaseCreator.class);
+    job.setMapperClass(PhaseCreatorMapper.class);
+    job.setMapOutputKeyClass(NullWritable.class);
+    job.setMapOutputValueClass(PhaseWritable.class);
+    job.setOutputKeyClass(NullWritable.class);
+    job.setOutputValueClass(PhaseWritable.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+    job.setInputFormatClass(SequenceFileOutputFormat.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    SequenceFileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
---
+++ mapreduce_maven/src/main/java/PhaseWritable.java
@@ -0,0 +1,101 @@
+
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.io.Serializable;
+
+import org.apache.hadoop.io.Writable;
+
+public class PhaseWritable implements Writable{
+
+
+    private Long duration;
+    private String patterns;
+    private int npatterns;
+    private String jobs;
+    private int njobs;
+    private String days;
+    private int ndays;
+
+    public PhaseWritable() { }
+
+    public Long getDuration() {
+        return duration;
+    }
+
+    public String getPatterns() {
+        return patterns;
+    }
+
+    public int getNpatterns() {
+        return npatterns;
+    }
+
+    public String getJobs() {
+        return jobs;
+    }
+
+    public int getNjobs() {
+        return njobs;
+    }
+
+    public String getDays() {
+        return days;
+    }
+
+    public int getNdays() {
+        return ndays;
+    }
+
+    public void setDuration(Long duration) {
+        this.duration = duration;
+    }
+
+    public void setPatterns(String patterns) {
+        this.patterns = patterns;
+    }
+
+    public void setNpatterns(int npatterns) {
+        this.npatterns = npatterns;
+    }
+
+    public void setJobs(String jobs) {
+        this.jobs = jobs;
+    }
+
+    public void setNjobs(int njobs) {
+        this.njobs = njobs;
+    }
+
+    public void setDays(String days) {
+        this.days = days;
+    }
+
+    public void setNdays(int ndays) {
+        this.ndays = ndays;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeLong(duration);
+        out.writeUTF(patterns);
+        out.writeInt(npatterns);
+        out.writeUTF(jobs);
+        out.writeInt(njobs);
+        out.writeUTF(days);
+        out.writeInt(ndays);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        duration = in.readLong();
+        patterns = in.readUTF();
+        npatterns = in.readInt();
+        jobs = in.readUTF();
+        njobs = in.readInt();
+        days = in.readUTF();
+        ndays = in.readInt();
+    }
+
+}
